79831a1698da4401b01225e223efb7c9261bd0dc
Asad Quraishi <aquraishi@knowledgework.ca>
2012-08-23 07:56:52 -0400

    Initial Commit

---
+++ Spinner Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Spinner Test
+//
+//  Created by Asad Quraishi on 2012-08-23.
+//  Copyright (c) 2012 Asad Quraishi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Spinner Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Spinner Test
+//
+//  Created by Asad Quraishi on 2012-08-23.
+//  Copyright (c) 2012 Asad Quraishi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
